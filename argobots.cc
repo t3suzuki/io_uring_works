a22007287e31af414c6cfdc3f43c4875095748ea
Tomoya Suzuki <t3suzuki@ucsd.edu>
2024-02-20 22:11:24 +0000

    up

--- argobots.cc
+++ argobots.cc
@@ -9,16 +9,16 @@
 
 #define TIME_SEC (10)
 
-#define N_CORE (8)
+#define N_CORE (4)
 #define N_ULT_PER_CORE (16)
 #define ULT_N_TH (N_CORE*N_ULT_PER_CORE)
 #define IO_URING_QD (N_ULT_PER_CORE*16)
 
-static struct io_uring ring[N_CORE][128];
-static ABT_xstream abt_xstreams[N_CORE];
+static struct io_uring ring[N_CORE+1][128];
+static ABT_xstream abt_xstreams[N_CORE+1];
 static ABT_thread abt_threads[ULT_N_TH];
-static ABT_pool global_abt_pools[N_CORE];
-static int done_flag[N_CORE][IO_URING_QD];
+static ABT_pool global_abt_pools[N_CORE+1];
+static int done_flag[N_CORE+1][IO_URING_QD];
 static int file_fd;
 
 typedef struct {
@@ -30,30 +30,30 @@ typedef struct {
 
 
 static inline
-void __io_uring_check2(int core_id)
+void __io_uring_check2(int ring_id)
 {
   struct io_uring_cqe *cqe;
   unsigned head;
   int i = 0;
-  io_uring_for_each_cqe(&ring[core_id][0], head, cqe) {
+  io_uring_for_each_cqe(&ring[ring_id][0], head, cqe) {
     if (cqe->res > 0) {
       //printf("%s %d %d\n", __func__, __LINE__, cqe->user_data);
-      done_flag[core_id][cqe->user_data] = 1;
+      done_flag[ring_id][cqe->user_data] = 1;
       i++;
     }
   }
   if (i > 0)
-    io_uring_cq_advance(&ring[core_id][0], i);
+    io_uring_cq_advance(&ring[ring_id][0], i);
 }
 
 static inline
-void __io_uring_bottom2(int core_id, int sqe_id)
+void __io_uring_bottom2(int ring_id, int sqe_id)
 {
-  io_uring_submit(&ring[core_id][0]);
+  io_uring_submit(&ring[ring_id][0]);
   while (1) {
-    __io_uring_check2(core_id);
+    __io_uring_check2(ring_id);
     //printf("%s %d %d\n", __func__, __LINE__, sqe_id);
-    if (done_flag[core_id][sqe_id])
+    if (done_flag[ring_id][sqe_id])
       break;
     ABT_thread_yield();
   }
@@ -95,7 +95,7 @@ wfunc(void *p)
   arg_t *arg = (arg_t *)p;
   int core_id = arg->core_id;
   void *buf;
-  const size_t sz = 512*1024*1024;
+  const size_t sz = 128*1024*1024;
   //const size_t sz = 128*1024;
   //const size_t sz = 4*1024;
   //printf("%s %d\n", __func__, __LINE__);
@@ -106,21 +106,20 @@ wfunc(void *p)
     if (*arg->quit) {
       break;
     }
-#if 0
-    size_t pos = (sz * count) % (512*1024*1024);
-    struct io_uring_sqe *sqe = io_uring_get_sqe(&ring[core_id][0]);
+    size_t pos = (sz * count) % (1ULL*1024*1024*1024);
+#if 1
+    int ring_id = N_CORE;
+    struct io_uring_sqe *sqe = io_uring_get_sqe(&ring[ring_id][0]);
     io_uring_prep_write(sqe, file_fd, buf, sz, pos);
-    int sqe_id = ((uint64_t)sqe - (uint64_t)ring[core_id][0].sq.sqes) / sizeof(struct io_uring_sqe);
+    int sqe_id = ((uint64_t)sqe - (uint64_t)ring[ring_id][0].sq.sqes) / sizeof(struct io_uring_sqe);
     sqe->user_data = sqe_id;
-    done_flag[core_id][sqe_id] = 0;
+    done_flag[ring_id][sqe_id] = 0;
     //printf("%s %d %lu %d %lu\n", __func__, __LINE__, count, sqe_id, pos);
-    __io_uring_bottom2(core_id, sqe_id);
+    __io_uring_bottom2(ring_id, sqe_id);
 #else
-    size_t pos = (sz * count) % (512*1024*1024);
-    
     size_t unit = 32768;
     size_t off;
-    for (off=0; off<count; off+=unit) {
+    for (off=0; off<sz; off+=unit) {
       struct io_uring_sqe *sqe = io_uring_get_sqe(&ring[core_id][0]);
       io_uring_prep_write(sqe, file_fd, (char *)buf+off, unit, pos+off);
       int sqe_id = ((uint64_t)sqe - (uint64_t)ring[core_id][0].sq.sqes) / sizeof(struct io_uring_sqe);
@@ -132,8 +131,7 @@ wfunc(void *p)
 #endif
     count ++;
   }
-  printf("%s %d\n", __func__, __LINE__);
-  
+  arg->count = count;  
 }
 
 void
@@ -168,7 +166,7 @@ int
 main(int argc, char **argv)
 {
   int i;
-  for (i=0; i<N_CORE; i++) {
+  for (i=0; i<N_CORE+1; i++) {
     io_uring_queue_init(IO_URING_QD, &ring[i][0], 0);
   }
   char *file_path = argv[1];
@@ -178,10 +176,10 @@ main(int argc, char **argv)
 
   ABT_init(0, NULL);
   ABT_xstream_self(&abt_xstreams[0]);
-  for (i=1; i<N_CORE; i++) {
+  for (i=1; i<N_CORE+1; i++) {
     ABT_xstream_create(ABT_SCHED_NULL, &abt_xstreams[i]);
   }
-  for (i=0; i<N_CORE; i++) {
+  for (i=0; i<N_CORE+1; i++) {
     ABT_xstream_set_cpubind(abt_xstreams[i], i);
     ABT_xstream_get_main_pools(abt_xstreams[i], 1, &global_abt_pools[i]);
   }
@@ -210,7 +208,7 @@ main(int argc, char **argv)
   ABT_thread wth;
   arg_t warg;
   {
-    int core_id = 0;
+    int core_id = N_CORE;
     warg.quit = &quit;
     warg.core_id = core_id;
     ABT_thread_create(global_abt_pools[core_id],
@@ -246,4 +244,5 @@ main(int argc, char **argv)
   double d_sec = (t2.tv_sec - t0.tv_sec) + (t2.tv_nsec - t0.tv_nsec) * 1e-9;
 
   printf("%f %lu %f KIOPS\n", d_sec, sum, sum / d_sec / 1000.0);
+  printf("write count %lu\n", warg.count);
 }
